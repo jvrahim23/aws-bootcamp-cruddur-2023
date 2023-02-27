3d8b2b48d59da5e17f4cc7563628543e1db58761
jwilson23 <jvrahim23@yahoo.com>
2023-02-27 16:25:14 +0000

    added tracer

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,17 +1,22 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
-      'created_at': (now - timedelta(days=2)).isoformat(),
-      'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
-      'replies': [{
+    with tracer.start_as_current_span("home-activities-mock-data"):
+      now = datetime.now(timezone.utc).astimezone()
+      results = [{
+        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=2)).isoformat(),
+        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'likes_count': 5,
+        'replies_count': 1,
+        'reposts_count': 0,
+        'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
@@ -20,8 +25,8 @@ class HomeActivities:
         'replies_count': 0,
         'reposts_count': 0,
         'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    },
+        }],
+      },
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
       'handle':  'Worf',
