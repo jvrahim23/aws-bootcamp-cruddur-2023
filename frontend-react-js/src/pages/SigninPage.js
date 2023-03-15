b9bd48ef78b3c2cfa48f12d73734754667964b49
jwilson23 <jvrahim23@yahoo.com>
2023-03-15 16:08:59 +0000

    updated the catch error section

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -30,19 +30,18 @@ export default function SigninPage() {
   const onsubmit = async (event) => {
     setErrors('')
     event.preventDefault();
-    try {
       Auth.signIn(email, password)
         .then(user => {
           localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
           window.location.href = "/"
         })
-    } catch (error) {
-      if (error.code == 'UserNotConfirmedException') {
-        window.location.href = "/confirm"
-      }
-      setErrors(error.message)
-    }
-    return false
+        .catch(error => { 
+          if (error.code == 'UserNotConfirmedException') {
+            window.location.href = "/confirm"
+         }
+         setErrors(error.message)
+        });
+      return false
   }
 
   const email_onchange = (event) => {
